bce817d3b0c19de90efddf437f4354fde11df63f
LuccaGarDal <luccagarciarosa@gmail.com>
2025-10-20 11:56:14 -0300

    PrintF (Especificadores de formato e sequências de escape)

---
+++ print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Sou Lucca, tenho 21 anos e gosta de programar em C!\n");
+    printf("Valor inteiro: %d\n", 42);
+    printf("Valor real: %.2f\n", 3.14159);
+    printf("Caractere: %c\n", 'A'); 
+    printf("String: %s\n", "Olá, Mundo!");
+    printf("Hexadecimal: %x\n", 255);
+    printf("Octal: %o\n", 255);
+    return 0;  
+
+}       
