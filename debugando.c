0bf063ebfb829b861741687aa5e429739ac082d8
LuccaGarDal <luccagarciarosa@gmail.com>
2025-10-20 11:02:42 -0300

    Debugando em C

---
+++ debugando.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(){
+    printf("Debugging C Program\n");
+    printf("Hello, World!\n");
+    printf("Mensagem 1\n");
+    printf("Mensagem 2\n");
+    printf("Mensagem 3\n");
+    printf("Mensagem 4\n");
+    printf("Mensagem 5\n");
+    
+    return 0;
+}
