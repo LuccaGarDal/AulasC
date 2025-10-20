5297fefbbeaf7b03f88e8e77f0da8c3dcfac1013
LuccaGarDal <luccagarciarosa@gmail.com>
2025-10-20 12:37:01 -0300

    ScanF

---
+++ scan.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    int idade = 0;
+    int ano = 0;
+
+    printf("A idade base : %d \n", idade);
+    printf("O ano base é : %d \n", ano);
+
+
+    printf("Digite sua idade e o ano de nascimento: \n");
+    scanf("%d" "%d", &idade, &ano);
+    
+    printf("A idade digitada foi: %d \n", idade);
+    printf("O ano de nascimento digitado foi: %d \n", ano);
+        
+}
