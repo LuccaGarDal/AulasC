24757dc58e05b86a6569eb81383c2334ea926e47
LuccaGarDal <luccagarciarosa@gmail.com>
2025-11-14 19:31:40 -0300

    litsa encadeada completa

--- listaEncadeada.c
+++ listaEncadeada.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Definindo estrutura do nó
 typedef struct Nodo {
     int valor;
     struct Nodo* prox;
 } Nodo;
 
+
+// Método para criar nós
 Nodo* criarNodo (int x) {
     Nodo* novo = (Nodo*) malloc(sizeof(Nodo));
     if (novo == NULL)
@@ -20,6 +23,7 @@ Nodo* criarNodo (int x) {
     
 }
 
+// Método para inserir nós na cabeça da fila
 Nodo* inserir (Nodo* inicio, int valor) {
     Nodo* novo = criarNodo(valor);
     if(inicio == NULL) {
@@ -36,17 +40,61 @@ Nodo* inserir (Nodo* inicio, int valor) {
     
 }
 
+// Método para consultar nós de acordo com o valor
+Nodo* consultar(Nodo* inicio, int valor) {
+    Nodo* atual = inicio;
+
+    while (atual != NULL) {
+        if (atual->valor == valor) {
+            return atual;
+        }
+        atual = atual->prox;
+    }
+
+    return NULL;
+}
+
+// Método para imprimir a lista
+void imprimir (Nodo* inicio) {
+    Nodo * atual = inicio;
+
+    while (atual != NULL) {
+        printf("%d -> ", atual->valor);
+        atual = atual->prox;   
+    }
+    printf("NULL\n");
+}
+
+// Método para remover um nó da lista
+Nodo* deletar (Nodo* inicio, int valor) {
+    if (inicio == NULL){
+        return NULL;
+    }
+    Nodo* atual = inicio;
+    Nodo* anterior = NULL;
+
+    if (atual->valor == valor){
+     Nodo* temp = atual->prox;
+     free(atual);   
+     return temp;
+
+    }
+
+    while (atual != NULL && atual-> valor != valor) {
+        anterior = atual;
+        atual = atual->prox;
+    }
+
+    if (atual == NULL) {
+        return inicio; 
+    }
+
+    anterior->prox = atual->prox;
+    free(atual);
+    
+    return inicio;
+}
+
 int main () {
-    Nodo* lista = NULL;
-
-    lista = inserir(lista, 10);
-    printf("Inseriu 10\n");
-
-    lista = inserir(lista, 20);
-    printf("Inseriu 20\n");
-
-    lista = inserir(lista, 30);
-    printf("Inseriu 30\n");
-
-    return 0;
+    
 }
